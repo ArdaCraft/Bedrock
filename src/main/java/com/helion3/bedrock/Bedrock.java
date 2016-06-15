d81fce8baef43a6523aa0f6bbeef55cef7c80491
dags <dags@dags.me>
2016-06-15 11:40:09 +0100

    Add build number to file & version

--- src/main/java/com/helion3/bedrock/Bedrock.java
+++ src/main/java/com/helion3/bedrock/Bedrock.java
@@ -48,8 +48,10 @@ import org.spongepowered.api.plugin.Plugin;
 
 import java.io.File;
 
-@Plugin(id = "bedrock", name = "bedrock", version = "1.0AC")
+@Plugin(id = "bedrock", name = "bedrock", version = Bedrock.VERSION)
 public class Bedrock {
+    public static final String VERSION = "${version}-build${build.number}";
+
     private static AFKManager afkManager;
     private static Configuration config;
     private static Game game;
